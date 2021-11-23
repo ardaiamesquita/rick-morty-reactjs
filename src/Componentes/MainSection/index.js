c2c070de4cd77dcc86b28714dd3a4239acef5896
ardaiamesquita <sofiaardaiamesquita@gmail.com>
2021-11-23 01:36:31 -0300

    Criando o componente principal

---
+++ src/Componentes/MainSection/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './style.css';
+function MainSection(){
+    return(
+        <div className="main-container"> 
+            <img src="https://www.proibidoler.com/wp-content/uploads/2020/07/critica-resenha-analise-rick-and-morty-3.jpg"
+            alt="rick-morty-img"
+            />
+            <div className="main-data-container">
+                <h1>Rick and Morty</h1>
+                <p>O brilhante clientista beberrão Rick sequestra Morty,
+                     seu neto aborrecente, para viver loucuras em outros
+                     mundos e dimensões alternativas.</p>
+                     <button type="subimit">Veja mais</button>
+            </div>
+            </div>
+
+    );
+}
+
+return default MainSection;
